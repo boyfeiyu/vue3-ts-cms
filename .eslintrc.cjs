2d10c7fe2fa1f853d36cf99c4a8f68204a6bc459
feiyu <296862383@qq.com>
2022-11-09 18:43:42 +0800

    配置eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,13 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
   }
 }
